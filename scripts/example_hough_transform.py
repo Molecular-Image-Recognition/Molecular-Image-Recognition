e4a89a2bc6c13d5fdcf82d19adb7b0cdd7beb636
Mark Payne <ampayne@mit.edu>
2017-10-05 21:21:53 -0400

    Rename 'Image' Directory to Avoid Confusion with Data

--- scripts/example_hough_transform.py
+++ scripts/example_hough_transform.py
@@ -1,6 +1,6 @@
 from skimage import io
 import os
-from images.hough_transform import hough_transform_plots
+from image_process.hough_transform import hough_transform_plots
 
 image = io.imread(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test', 'hexagon.JPG'),
                   as_grey=True)
