7e403f4eb48f41ffe98ccb8930d0cc8f22e5da95
Matt Johnson <mjohnson541@gmail.com>
2017-11-22 10:50:11 -0500

    adjustments to the example script

--- scripts/example_picture_to_graph.py
+++ scripts/example_picture_to_graph.py
@@ -6,29 +6,55 @@ from regression.lines_to_graph import *
 from molecule.adjacency import *
 from lines.line import *
 import matplotlib.pyplot as plt
-image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test', 'hexagon.JPG')
+from copy import deepcopy
+
+#image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test', 'hexagon.JPG')
 
 #image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test', 'Test_Set_1', 'PNGs',
- #                    'C(C)C(CCCC)(C)C.png')
+#                     'C(C)C(CCCC)(C)C.png')
 
 #image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test','square.jpg' )
+#image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test', 'Test_Set_1','double_bonds', 'C((CCC)CC)C.png')
+image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'hough_test', 'Test_Set_1','double_bonds', 'C(C)CC.png')
 
 lines = get_hough_lines(image)
+
+lines_final = deepcopy(lines)
+#for line in lines_final:
+#    plt.plot((line.pts[0].x, line.pts[1].x), (line.pts[0].y, line.pts[1].y))
+#plt.xlim((0, get_image_size(image)[1]))
+#plt.ylim(( get_image_size(image)[0],0))
+#plt.figure()
 #imsize = min(get_image_size(image))
 
 imdim = max([x.length for x in lines])
 #imdim = np.median(np.array([x.length for x in lines]))
 
 # normalize all the lines
-for i,l in enumerate(lines):
+#for i,l in enumerate(lines):
+#    pt1 = l.pts[0]
+#    pt2 = l.pts[1]
+#    pt1.x /= imdim
+#    pt2.x /= imdim
+#    pt1.y /= imdim
+#    pt2.y /= imdim
+#    lines[i] = LineSegment([pt1,pt2])
+#    
+#    
+#lines1 = lines_to_graph(lines, [0.2,0.4,1.0,0.2,0.5,0.5,0.5,1.0])
+#
+#imdim *= max([x.length for x in lines1])
+#imdim = np.median(np.array([x.length for x in lines]))
+
+# normalize all the lines
+for i,l in enumerate(lines_final):
     pt1 = l.pts[0]
     pt2 = l.pts[1]
     pt1.x /= imdim
     pt2.x /= imdim
     pt1.y /= imdim
     pt2.y /= imdim
-    lines[i] = LineSegment([pt1,pt2])
-    
+    lines_final[i] = LineSegment([pt1,pt2])
     
 #    min_dist_merge = params[0]
 #    min_angle_merge = params[1]
@@ -38,13 +64,15 @@ for i,l in enumerate(lines):
 #    max_dist_bond = params[5]
 #    max_angle_bond = params[6]
 #    node_radius = params[7]
-lines = lines_to_graph(lines, [1.0,.8,.1,0.1,0.5,0.5,0.5,.5])
+lines = lines_to_graph(lines_final, [0.05,.4,.1,0.2,0.06,0.8,0.1,.2])
+#lines = lines_to_graph(lines_final, [0.05,.4,.1,0.2,0.06,0.8,0.1,.2])
+print [line.order for line in lines]
 for line in lines:
     plt.plot((line.pts[0].x, line.pts[1].x), (line.pts[0].y, line.pts[1].y))
 plt.xlim((0, get_image_size(image)[1]/imdim))
 plt.ylim(( get_image_size(image)[0]/imdim,0))
 plt.figure()
-lines = combinePoints(lines,0.5)
+
 for line in lines:
     plt.plot((line.pts[0].x, line.pts[1].x), (line.pts[0].y, line.pts[1].y))
 plt.xlim((0, get_image_size(image)[1]/imdim))
@@ -52,9 +80,12 @@ plt.ylim(( get_image_size(image)[0]/imdim,0))
 
 plt.title('Probabilistic Hough')
 
-adj = getAdjMatrix(lines,node_radius)
+adj = getAdjMatrix(lines)
+print adj
 atomnames = ['C']*adj.shape[0]
 molecule = Adjacency(adj, atomnames)
 molecule.addHydrogens()
 rmgmol = molecule.toRMGmol()
+print rmgmol.toSMILES()
+
 
